572d02a80e3412cafc0995538941caac2cfcb7b8
Cameron <campage1999@gmail.com>
2026-03-14 21:04:22 +1030

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.36.0"
+    }
+  }
+}
+
 resource "aws_ec2_host" "webapp_host" {
   availability_zone = "ap-southeast-2"
   instance_type     = "m3.micro"
